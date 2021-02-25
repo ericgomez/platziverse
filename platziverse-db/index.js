853ab5bde96bc5188ed22bf8d62f52f20ae627b5
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-02-25 17:43:20 -0600

    [db] add basic tests with Ava

--- platziverse-db/index.js
+++ platziverse-db/index.js
@@ -3,8 +3,21 @@
 const setupDatabase = require('./lib/db')
 const setupAgentModel = require('./models/agent')
 const setupMetricModel = require('./models/metric')
+const defaults = require('defaults')
 
 module.exports = async function (config) {
+  config = defaults(config, {
+    dialect: 'sqlite',
+    pool: {
+      max: 10,
+      min: 0,
+      idle: 10000
+    },
+    query: {
+      raw: true
+    }
+  })
+
   const sequelize = setupDatabase(config)
   const AgentModel = setupAgentModel(config)
   const MetricModel = setupMetricModel(config)
@@ -17,7 +30,7 @@ module.exports = async function (config) {
   if (config.setup) {
     await sequelize.sync({ force: true })
   }
-  
+
   const Agent = {}
   const Metric = {}
 
